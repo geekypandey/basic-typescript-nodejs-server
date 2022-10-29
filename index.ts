df6b31557b2f0f25de48ad8a85f1a69962187cee
Anurag Pandey <anuragcarbon1998@gmail.com>
2022-10-29 18:54:37 +0530

    move to typescript

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
 
 dotenv.config();
 
-const app = express();
+const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
 	res.send('Express + Typescript server');
 });
 
